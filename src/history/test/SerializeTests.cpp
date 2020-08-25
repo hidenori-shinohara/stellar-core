86800d1530cb89be8a1aa1b6df0deb598d000fc8
Hidenori <hidenori.shinohara.jp@gmail.com>
2020-08-25 16:33:44 -0400

    Add version-2 files to the round trip tests
    This ensures that the version-1 HAS code can handle version-2 json input
    files.

--- src/history/test/SerializeTests.cpp
+++ src/history/test/SerializeTests.cpp
@@ -12,23 +12,34 @@ using namespace stellar;
 
 TEST_CASE("Serialization round trip", "[history]")
 {
-    std::vector<std::string> testFiles = {
+    std::vector<std::string> inputFiles = {
         "stellar-history.testnet.6714239.json",
-        "stellar-history.livenet.15686975.json"};
-    for (auto const& fn : testFiles)
+        "stellar-history.livenet.15686975.json",
+        "stellar-history.testnet.6714239.version2.input.json"};
+    std::vector<std::string> outputFiles = {
+        "stellar-history.testnet.6714239.json",
+        "stellar-history.livenet.15686975.json",
+        "stellar-history.testnet.6714239.version2.output.json"};
+    for (int i = 0; i < inputFiles.size(); i++)
     {
         std::string fnPath = "testdata/";
-        fnPath += fn;
-        SECTION("Serialize " + fnPath)
+        std::string inputPath = fnPath + inputFiles[i];
+        std::string outputPath = fnPath + outputFiles[i];
+        SECTION("Serialize " + inputPath)
         {
-            std::ifstream in(fnPath);
+            std::ifstream in(inputPath);
             in.exceptions(std::ios::badbit);
-            std::string fromFile((std::istreambuf_iterator<char>(in)),
+            std::string input((std::istreambuf_iterator<char>(in)),
+                                 std::istreambuf_iterator<char>());
+
+            std::ifstream out(outputPath);
+            out.exceptions(std::ios::badbit);
+            std::string output((std::istreambuf_iterator<char>(out)),
                                  std::istreambuf_iterator<char>());
 
             HistoryArchiveState has;
-            has.fromString(fromFile);
-            REQUIRE(fromFile == has.toString());
+            has.fromString(input);
+            REQUIRE(output == has.toString());
         }
     }
 }
